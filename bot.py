cd89f70e83b706041d0b093cc5fbf0643ab65c9d
mantaspaulauskas1997-stack <mantaspaulauskas1997@gmail.com>
2026-06-18 19:51:14 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -901,9 +901,9 @@ async def verify_valorant_account(message: discord.Message, riot_id: str):
                     f"🕒 Tikslus laikas: <t:{next_verify_at}:F>",
                     mention_author=False
                 )
-                return
+                                return
 
-                        async with message.channel.typing():
+        async with message.channel.typing():
             rank, rr, elo = await fetch_valorant_rank(name, tag)
             base_rank, role = await update_valorant_rank_role(
                 message.guild,
