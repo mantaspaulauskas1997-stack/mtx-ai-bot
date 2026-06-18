c65d669b7e8b5de8d8fc44e9b86e3898aa12e245
mantaspaulauskas1997-stack <mantaspaulauskas1997@gmail.com>
2026-06-18 19:10:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1454,12 +1454,14 @@ async def rotate_status():
 
 @bot.event
 async def on_ready():
-print(f"✅ Botas prisijungė: {bot.user}")
-print(f"✅ Serverių kiekis: {len(bot.guilds)}")
-print("VALORANT DATA:", load_valorant_links())
-print("==============================")
+    print("==============================")
+    print(f"✅ Botas prisijungė: {bot.user}")
+    print(f"✅ Serverių kiekis: {len(bot.guilds)}")
+    print("VALORANT DATA:", load_valorant_links())
+
     if not rotate_status.is_running():
         rotate_status.start()
+
     for guild in bot.guilds:
         print(f"📌 Serveris: {guild.name} | ID: {guild.id}")
 
