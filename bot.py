726e0c6d66edae573e0f83d84bfa013c0f50560a
mantaspaulauskas1997-stack <mantaspaulauskas1997@gmail.com>
2026-06-18 16:43:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -271,12 +271,14 @@ bot = commands.Bot(
 )
 @tasks.loop(seconds=30)
 async def rotate_status():
-   statuses = [
-    "👥 Prižiūriu serverį",
-    "🎮 Valorant",
-    "📢 /help",
-    f"👥 {bot.guilds[0].member_count} narių"
-]
+    member_count = bot.guilds[0].member_count if bot.guilds else 0
+
+    statuses = [
+        "🎩 Prižiūriu serverį",
+        "🎮 Valorant",
+        "📢 /help",
+        f"👥 {member_count} narių"
+    ]
 
     status = statuses[rotate_status.current_loop % len(statuses)]
 
